7964959f82d1679e8cf1292355912472ce5b1dda
SkyMage0 <116276180+SkyMage0@users.noreply.github.com>
2025-10-01 18:26:34 +0900

    Create currency.rs

---
+++ constants/currency.rs
@@ -0,0 +1,5 @@
+// constants/currency.rs
+pub const PLANCK: u128 = 1;
+pub const MICROUNIT: u128 = 1_000_000 * PLANCK;
+pub const MILLIUNIT: u128 = 1_000 * MICROUNIT;
+pub const UNIT: u128 = 1_000 * MILLIUNIT;
