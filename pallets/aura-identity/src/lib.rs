e95ab3ca93876ef32b8550a067d79c2524f002e5
SkyMage0 <116276180+SkyMage0@users.noreply.github.com>
2025-10-01 13:34:32 +0900

    Update lib.rs
    style: format code with rustfmt standards

--- pallets/aura-identity/src/lib.rs
+++ pallets/aura-identity/src/lib.rs
@@ -4,222 +4,214 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{pallet_prelude::*, Blake2_128Concat};
-    use frame_system::pallet_prelude::*;
-    use sp_std::vec::Vec;
+	use frame_support::{pallet_prelude::*, Blake2_128Concat};
+	use frame_system::pallet_prelude::*;
+	use sp_std::vec::Vec;
 
-    #[pallet::pallet]
-    pub struct Pallet<T>(_);
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
 
-    #[pallet::config]
-    pub trait Config: frame_system::Config {
-        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-    }
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
 
-    #[pallet::storage]
-    #[pallet::getter(fn get_aura_id)]
-    pub type AuraIdentities<T: Config> = StorageMap<
-        _,
-        Blake2_128Concat,
-        T::AccountId,
-        AuraIdRecord,
-    >;
+	#[pallet::storage]
+	#[pallet::getter(fn get_aura_id)]
+	pub type AuraIdentities<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, AuraIdRecord>;
 
-    #[pallet::storage]
-    pub type DidIndex<T: Config> = StorageMap<
-        _,
-        Blake2_128Concat,
-        [u8; 32],
-        T::AccountId,
-    >;
+	#[pallet::storage]
+	pub type DidIndex<T: Config> = StorageMap<_, Blake2_128Concat, [u8; 32], T::AccountId>;
 
-    #[pallet::event]
-    #[pallet::generate_deposit(pub(super) fn deposit_event)]
-    pub enum Event<T: Config> {
-        AuraIdCreated { account: T::AccountId, did: [u8; 32] },
-        RecoverySetup { account: T::AccountId, shares: u8 },
-    }
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		AuraIdCreated { account: T::AccountId, did: [u8; 32] },
+		RecoverySetup { account: T::AccountId, shares: u8 },
+	}
 
-    #[pallet::error]
-    pub enum Error<T> {
-        AuraIdAlreadyExists,
-        AuraIdNotFound,
-    }
+	#[pallet::error]
+	pub enum Error<T> {
+		AuraIdAlreadyExists,
+		AuraIdNotFound,
+	}
 
-    #[pallet::call]
-    impl<T: Config> Pallet<T> {
-        #[pallet::call_index(0)]
-        #[pallet::weight(10_000)]
-        pub fn create_aura_id(
-            origin: OriginFor<T>,
-            public_key: [u8; 32],
-            recovery_config: Vec<u8>,
-        ) -> DispatchResult {
-            let who = ensure_signed(origin)?;
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::call_index(0)]
+		#[pallet::weight(10_000)]
+		pub fn create_aura_id(
+			origin: OriginFor<T>,
+			public_key: [u8; 32],
+			recovery_config: Vec<u8>,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
 
-            ensure!(!AuraIdentities::<T>::contains_key(&who), Error::<T>::AuraIdAlreadyExists);
+			ensure!(!AuraIdentities::<T>::contains_key(&who), Error::<T>::AuraIdAlreadyExists);
 
-            let did = Self::generate_did(&public_key);
-            
-            let record = AuraIdRecord {
-                did,
-                public_key,
-                recovery_config,
-                created: frame_system::Pallet::<T>::block_number(),
-            };
+			let did = Self::generate_did(&public_key);
 
-            AuraIdentities::<T>::insert(&who, record.clone());
-            DidIndex::<T>::insert(did, &who);
+			let record = AuraIdRecord {
+				did,
+				public_key,
+				recovery_config,
+				created: frame_system::Pallet::<T>::block_number(),
+			};
 
-            Self::deposit_event(Event::AuraIdCreated { account: who, did });
-            
-            Ok(())
-        }
+			AuraIdentities::<T>::insert(&who, record.clone());
+			DidIndex::<T>::insert(did, &who);
 
-        #[pallet::call_index(1)]
-        #[pallet::weight(10_000)]
-        pub fn setup_recovery(
-            origin: OriginFor<T>,
-            shares: u8,
-            trustees: Vec<T::AccountId>,
-        ) -> DispatchResult {
-            let who = ensure_signed(origin)?;
+			Self::deposit_event(Event::AuraIdCreated { account: who, did });
 
-            // TODO: Implement social recovery logic
-            Self::deposit_event(Event::RecoverySetup { account: who, shares });
-            
-            Ok(())
-        }
-    }
+			Ok(())
+		}
 
-    impl<T: Config> Pallet<T> {
-        fn generate_did(public_key: &[u8; 32]) -> [u8; 32] {
-            sp_io::hashing::blake2_256(public_key)
-        }
-    }
+		#[pallet::call_index(1)]
+		#[pallet::weight(10_000)]
+		pub fn setup_recovery(
+			origin: OriginFor<T>,
+			shares: u8,
+			trustees: Vec<T::AccountId>,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
 
-    #[derive(Clone, Encode, Decode, PartialEq, TypeInfo)]
-    #[scale_info(skip_type_params(T))]
-    pub struct AuraIdRecord {
-        pub did: [u8; 32],
-        pub public_key: [u8; 32],
-        pub recovery_config: Vec<u8>,
-        pub created: T::BlockNumber,
-    }
+			// TODO: Implement social recovery logic
+			Self::deposit_event(Event::RecoverySetup { account: who, shares });
+
+			Ok(())
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+		fn generate_did(public_key: &[u8; 32]) -> [u8; 32] {
+			sp_io::hashing::blake2_256(public_key)
+		}
+	}
+
+	#[derive(Clone, Encode, Decode, PartialEq, TypeInfo)]
+	#[scale_info(skip_type_params(T))]
+	pub struct AuraIdRecord {
+		pub did: [u8; 32],
+		pub public_key: [u8; 32],
+		pub recovery_config: Vec<u8>,
+		pub created: T::BlockNumber,
+	}
 }
 
-// ТЕСТЫ
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use frame_support::{assert_ok, assert_noop};
-    use sp_core::H256;
-    use sp_runtime::{traits::{BlakeTwo256, IdentityLookup}, testing::Header};
+	use super::*;
+	use frame_support::{assert_noop, assert_ok};
+	use sp_core::H256;
+	use sp_runtime::{
+		testing::Header,
+		traits::{BlakeTwo256, IdentityLookup},
+	};
 
-    type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-    type Block = frame_system::mocking::MockBlock<Test>;
+	type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+	type Block = frame_system::mocking::MockBlock<Test>;
 
-    frame_support::construct_runtime!(
-        pub enum Test where
-            Block = Block,
-            NodeBlock = Block,
-            UncheckedExtrinsic = UncheckedExtrinsic,
-        {
-            System: frame_system,
-            AuraIdentity: pallet,
-        }
-    );
+	frame_support::construct_runtime!(
+		pub enum Test where
+			Block = Block,
+			NodeBlock = Block,
+			UncheckedExtrinsic = UncheckedExtrinsic,
+		{
+			System: frame_system,
+			AuraIdentity: pallet,
+		}
+	);
 
-    impl frame_system::Config for Test {
-        type BaseCallFilter = frame_support::traits::Everything;
-        type BlockWeights = ();
-        type BlockLength = ();
-        type DbWeight = ();
-        type RuntimeOrigin = RuntimeOrigin;
-        type RuntimeCall = RuntimeCall;
-        type Nonce = u64;
-        type Hash = H256;
-        type Hashing = BlakeTwo256;
-        type AccountId = u64;
-        type Lookup = IdentityLookup<Self::AccountId>;
-        type Block = Block;
-        type RuntimeEvent = RuntimeEvent;
-        type BlockHashCount = frame_system::limits::BlockHashCount;
-        type Version = ();
-        type PalletInfo = PalletInfo;
-        type AccountData = ();
-        type OnNewAccount = ();
-        type OnKilledAccount = ();
-        type SystemWeightInfo = ();
-        type SS58Prefix = ();
-        type OnSetCode = ();
-        type MaxConsumers = frame_support::traits::ConstU32<16>;
-    }
+	impl frame_system::Config for Test {
+		type BaseCallFilter = frame_support::traits::Everything;
+		type BlockWeights = ();
+		type BlockLength = ();
+		type DbWeight = ();
+		type RuntimeOrigin = RuntimeOrigin;
+		type RuntimeCall = RuntimeCall;
+		type Nonce = u64;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Block = Block;
+		type RuntimeEvent = RuntimeEvent;
+		type BlockHashCount = frame_system::limits::BlockHashCount;
+		type Version = ();
+		type PalletInfo = PalletInfo;
+		type AccountData = ();
+		type OnNewAccount = ();
+		type OnKilledAccount = ();
+		type SystemWeightInfo = ();
+		type SS58Prefix = ();
+		type OnSetCode = ();
+		type MaxConsumers = frame_support::traits::ConstU32<16>;
+	}
 
-    impl Config for Test {
-        type RuntimeEvent = RuntimeEvent;
-    }
+	impl Config for Test {
+		type RuntimeEvent = RuntimeEvent;
+	}
 
-    #[test]
-    fn test_did_generation() {
-        let public_key = [1u8; 32];
-        let did = pallet::Pallet::<Test>::generate_did(&public_key);
-        
-        // DID должен быть хэшом публичного ключа
-        assert_eq!(did.len(), 32);
-        assert_ne!(did, public_key); // Должен отличаться от исходного ключа
-    }
+	#[test]
+	fn test_did_generation() {
+		let public_key = [1u8; 32];
+		let did = pallet::Pallet::<Test>::generate_did(&public_key);
 
-    #[test]
-    fn test_create_aura_id() {
-        new_test_ext().execute_with(|| {
-            let account_id = 1;
-            let public_key = [2u8; 32];
-            let recovery_config = vec![1, 2, 3];
+		// DID должен быть хэшом публичного ключа
+		assert_eq!(did.len(), 32);
+		assert_ne!(did, public_key); // Должен отличаться от исходного ключа
+	}
 
-            // Создание Aura ID должно работать
-            assert_ok!(AuraIdentity::create_aura_id(
-                RuntimeOrigin::signed(account_id),
-                public_key,
-                recovery_config.clone()
-            ));
+	#[test]
+	fn test_create_aura_id() {
+		new_test_ext().execute_with(|| {
+			let account_id = 1;
+			let public_key = [2u8; 32];
+			let recovery_config = vec![1, 2, 3];
 
-            // Проверяем что запись создалась
-            let record = AuraIdentity::get_aura_id(account_id).unwrap();
-            assert_eq!(record.public_key, public_key);
-            assert_eq!(record.recovery_config, recovery_config);
-        });
-    }
+			// Создание Aura ID должно работать
+			assert_ok!(AuraIdentity::create_aura_id(
+				RuntimeOrigin::signed(account_id),
+				public_key,
+				recovery_config.clone()
+			));
 
-    #[test]
-    fn test_duplicate_aura_id() {
-        new_test_ext().execute_with(|| {
-            let account_id = 1;
-            let public_key = [3u8; 32];
-            let recovery_config = vec![1, 2, 3];
+			// Проверяем что запись создалась
+			let record = AuraIdentity::get_aura_id(account_id).unwrap();
+			assert_eq!(record.public_key, public_key);
+			assert_eq!(record.recovery_config, recovery_config);
+		});
+	}
 
-            // Первое создание - ок
-            assert_ok!(AuraIdentity::create_aura_id(
-                RuntimeOrigin::signed(account_id),
-                public_key,
-                recovery_config.clone()
-            ));
+	#[test]
+	fn test_duplicate_aura_id() {
+		new_test_ext().execute_with(|| {
+			let account_id = 1;
+			let public_key = [3u8; 32];
+			let recovery_config = vec![1, 2, 3];
 
-            // Второе создание для того же аккаунта - ошибка
-            assert_noop!(
-                AuraIdentity::create_aura_id(
-                    RuntimeOrigin::signed(account_id),
-                    public_key,
-                    recovery_config
-                ),
-                Error::<Test>::AuraIdAlreadyExists
-            );
-        });
-    }
+			// Первое создание - ок
+			assert_ok!(AuraIdentity::create_aura_id(
+				RuntimeOrigin::signed(account_id),
+				public_key,
+				recovery_config.clone()
+			));
 
-    // Вспомогательная функция для тестов
-    fn new_test_ext() -> sp_io::TestExternalities {
-        let t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
-        t.into()
-    }
+			// Второе создание для того же аккаунта - ошибка
+			assert_noop!(
+				AuraIdentity::create_aura_id(
+					RuntimeOrigin::signed(account_id),
+					public_key,
+					recovery_config
+				),
+				Error::<Test>::AuraIdAlreadyExists
+			);
+		});
+	}
+
+	// Вспомогательная функция для тестов
+	fn new_test_ext() -> sp_io::TestExternalities {
+		let t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+		t.into()
+	}
 }
